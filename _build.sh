8655ebef6b7070b4ba12868d48ea1090122d2709
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-07-19 01:57:02 +0700

    del

--- _build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export DOCKER_SCAN_SUGGEST=false
-docker build -t dinhgiang/share-tools-frontend . && \
-docker tag dinhgiang/share-tools-frontend:latest 025340181271.dkr.ecr.us-west-2.amazonaws.com/dinhgiang/share-tools-frontend:latest && \
-docker push 025340181271.dkr.ecr.us-west-2.amazonaws.com/dinhgiang/share-tools-frontend:latest
